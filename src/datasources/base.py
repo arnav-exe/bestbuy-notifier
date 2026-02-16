51ee300443fa857d7de94fc332087a4dc9febe94
arnav-exe <arnav.jagetia03@gmail.com>
2026-02-16 14:21:57 -0500

    cleaup

--- src/datasources/base.py
+++ src/datasources/base.py
@@ -15,24 +15,11 @@ class DataSource(ABC):
         # parse raw data into compatible format
         pass
 
-    def fetch_product(identifier: str):
+    def fetch_product(self, identifier: str):
         # main method for fetching and parsing data
-        # data = self.fetch_raw(identifier).json()
-        #
-        # return self.parse(data)  # returns Product obj (as defined in schema.py)
-        from ..schema import Product
+        data = self.fetch_raw(identifier).json()
 
-        return Product(
-            "B0FQFB8FMG",
-            "airpods pro 3",
-            True,
-            True,
-            199.99,
-            249.99,
-            "https://amazon.com/dp/airpods-pro-3",
-            "amazon",
-            "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
-        )
+        return self.parse(data)  # returns Product obj (as defined in schema.py)
 
     def can_handle(self, retailer_name):
         return self.source_name == retailer_name
